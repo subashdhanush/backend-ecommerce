1f1951948127d3890d6db43a1368987802123fd6
subashdhanush <subashdhanush1618@gmail.com>
2022-05-22 13:15:08 +0530

    commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose")
+
+// var mongoURL='mongodb+srv://subash_03:12345@cluster0.brumh.mongodb.net/test'
+var mongoURL=process.env.MONGO_URL
+
+mongoose.connect(mongoURL,{useUnifiedTopology:true,useNewUrlParser:true})
+
+var db=mongoose.connection
+
+db.on('connected',()=>{
+  console.log("Mongo DB Connection Successfull");  
+})
+
+db.on('error',()=>{
+  console.log("Mongo DB Connection failed");  
+})
+
+module.exports=mongoose
